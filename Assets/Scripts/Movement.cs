21cff29d5a37d71c949bbefe72288420cc3c3c94
JGMelon22 <jgvasconcellos22@gmail.com>
2023-09-16 16:02:56 -0300

    feat: desenvolvendo movimento horizontal do jogador

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -2,13 +2,21 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    private float _inputHorizontal;
+    private readonly float _playerSpeed = 25F;
+    private Rigidbody2D _rigidbody;
+
     // Start is called before the first frame update
     private void Start()
     {
+        _rigidbody = gameObject.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     private void Update()
     {
+        // Movimentação do player
+        _inputHorizontal = Input.GetAxis("Horizontal");
+        _rigidbody.AddForce(new Vector2(_inputHorizontal * _playerSpeed, 0));
     }
 }
