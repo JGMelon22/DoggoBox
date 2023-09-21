a42202730335d651a6e7d241ce5ecd8cbc1e3ade
JGMelon22 <jgvasconcellos22@gmail.com>
2023-09-21 18:18:40 -0300

    fix: removendo estado de player_lose

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,10 +4,10 @@ using UnityEngine.SceneManagement;
 public class Movement : MonoBehaviour
 {
     [SerializeField] private ObjectBehaviour _objectBehaviour;
+    [SerializeField] private Animator _animator;
     private readonly float _playerSpeed = 35F;
     private float _inputHorizontal;
     private Rigidbody2D _rigidbody;
-    [SerializeField] private Animator _animator;
 
     // Start is called before the first frame update
     private void Start()
@@ -30,9 +30,10 @@ public class Movement : MonoBehaviour
         _inputHorizontal = Input.GetAxisRaw("Horizontal");
 
         if (_inputHorizontal != 0) _rigidbody.AddForce(new Vector2(_inputHorizontal * _playerSpeed, 0));
-        
+
         // Animação do player
-        _animator.SetFloat("Speed", Mathf.Abs(_inputHorizontal)); // Usamos Abs. devido ao eixo x na esquerda ser negativo
+        _animator.SetFloat("Speed",
+            Mathf.Abs(_inputHorizontal)); // Usamos Abs. devido ao eixo x na esquerda ser negativo
     }
 
     // Reiniciar jogo
--- Assets/Scripts/ObjectBehaviour.cs
+++ Assets/Scripts/ObjectBehaviour.cs
@@ -5,17 +5,9 @@ using Random = System.Random;
 public class ObjectBehaviour : MonoBehaviour
 {
     [SerializeField] private GameObject prefab;
-    private bool _isGameOver = false;
-    
-    // Gera presente em um lugar aleatório 
-    public void SpawnObject()
-    {
-        var random = new Random();
-        Instantiate(prefab, new Vector3(random.Next(-10, 10), 3.9F, 0F), Quaternion.identity);
-    }
-    
+    private bool _isGameOver;
+
     // Obtem o presente ou se cair no chão, game over
-    [MenuItem("Custom Menu/Play")]
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player") && !_isGameOver)
@@ -23,12 +15,19 @@ public class ObjectBehaviour : MonoBehaviour
             SpawnObject();
             Destroy(gameObject);
         }
-        
+
         else if (collision.gameObject.CompareTag("Ground"))
         {
             _isGameOver = true;
             EditorUtility.DisplayDialog("Alerta", "Game over! Tecle \"Esc\" para sair ou \"R\" para recomeçar", "Ok");
             Debug.Log("GAME OVER!");
         }
+    }
+
+    // Gera presente em um lugar aleatório 
+    public void SpawnObject()
+    {
+        var random = new Random();
+        Instantiate(prefab, new Vector3(random.Next(-10, 10), 3.9F, 0F), Quaternion.identity);
     }
 }
