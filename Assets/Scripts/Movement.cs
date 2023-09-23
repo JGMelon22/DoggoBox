8ee3c5ac3f6f0aa685abe3bb566881d65c8b1657
JGMelon22 <jgvasconcellos22@gmail.com>
2023-09-23 13:10:04 -0300

    feat: melhorando a implementação de flip do jogador

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -6,6 +6,7 @@ public class Movement : MonoBehaviour
     [SerializeField] private ObjectBehaviour _objectBehaviour;
     [SerializeField] private Animator _animator;
     private readonly float _playerSpeed = 35F;
+    private bool _facingRight = true;
     private float _inputHorizontal;
     private Rigidbody2D _rigidbody;
 
@@ -29,18 +30,33 @@ public class Movement : MonoBehaviour
         // Movimentação do player
         _inputHorizontal = Input.GetAxisRaw("Horizontal");
 
-        if (_inputHorizontal != 0) _rigidbody.AddForce(new Vector2(_inputHorizontal * _playerSpeed, 0));
-        
+        if (_inputHorizontal != 0)
+            _rigidbody
+                .AddForce(new Vector2(_inputHorizontal * _playerSpeed, 0));
+
         // Flip player
-        if (_inputHorizontal > 0)
-            gameObject.transform.localScale = new Vector3(0.27F, 0.27F, 1);
-        
-        else if (_inputHorizontal < 0)
-            gameObject.transform.localScale = new Vector3(-0.27F, 0.27F, 1);
-        
+        if (_inputHorizontal > 0 && !_facingRight)
+            Flip();
+
+        else if (_inputHorizontal < 0 && _facingRight)
+            Flip();
+
         // Animação do player
         _animator.SetFloat("Speed",
             Mathf.Abs(_inputHorizontal)); // Usamos Abs. devido ao eixo x na esquerda ser negativo
+    }
+
+    /// <summary>
+    ///     Obtem a scale atual do jogador
+    ///     e muda para o sentido oposto
+    /// </summary>
+    private void Flip()
+    {
+        var currentScale = gameObject.transform.localScale;
+        currentScale.x *= -1;
+        gameObject.transform.localScale = currentScale;
+
+        _facingRight = !_facingRight;
     }
 
     // Reiniciar jogo
